b416a02407a53ad77954303f6defbc46b1b45b45
Minh Tu <minhtu.bk14@gmail.com>
2025-04-16 10:01:37 +0700

    add

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"18b71d647a292a980abb405ac7d16fe1f0b204
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2010924806"
+    serviceWorkerVersion: "1404013253"
   }
 });
